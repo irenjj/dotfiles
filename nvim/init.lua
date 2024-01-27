bee582facc1347ff43c8bb64066264b965eac40b
irenjj <renj.jiang@gmail.com>
2024-01-27 15:12:27 +0800

    feat: nvim remove lsp lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -145,37 +145,139 @@ require("lazy").setup({
         end
     },
 
-    -- Git
+    -- Gitsigns
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup({})
+            require("gitsigns").setup()
         end
     },
 
-    -- LSP
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/vim-vsnip" },
-    { "onsails/lspkind-nvim" },
-    { "hrsh7th/cmp-vsnip" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "mortepau/codicons.nvim" },
+   -- LSP
+   {
+       "neovim/nvim-lspconfig",
+       dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-nvim-lsp",
+       },
+       config = function()
+            -- Mason 
+            require("mason").setup()
+
+            -- Mason-lspconfig
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+            })
+
+            -- Lsp keybindings
+            local function on_attach(client, bufnr)
+                client.server_capabilities.documentFormattingProvider = false
+                client.server_capabilities.documentRangeFormattingProvider = false
+                local function buf_set_keymap(...)
+                    vim.api.nvim_buf_set_keymap(bufnr, ...)
+                end
+                require("keybindings").mapLSP(buf_set_keymap)
+                -- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format { async = true}")
+            end
+
+            -- Lua lsp
+            require("lspconfig").lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                            version = "LuaJIT",
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+                flags = {
+                    debounce_text_changes = 160,
+                },
+                on_attach = on_attach,
+           })
+
+           -- Rust lsp
+           require("lspconfig").rust_analyzer.setup({
+               settings = {
+                   ["rust-analyzer"] = {
+                       imports = {
+                           granularity = {
+                               group = "module",
+                           },
+                           prefix = "self",
+                       },
+                       cargo = {
+                           buildScripts = {
+                               enable = true,
+                           },
+                       },
+                       procMacro = {
+                           enable = true,
+                       },
+                   },
+               },
+               on_attach = on_attach,
+           })
+
+           -- Cmp
+           local cmp = require("cmp")
+           cmp.setup({
+               snippet = {
+                   expand = function(args)
+                       -- For `vsnip` users.
+                       vim.fn["vsnip#anonymous"](args.body)
+                   end,
+               },
+               sources = cmp.config.sources({
+                   { name = "nvim_lsp" },
+                   -- For vsnip users.
+                   { name = "vsnip" },
+               }, { { name = "buffer" }, { name = "path" } }),
+               mapping = require("keybindings").cmp(cmp),
+           })
+       end
+    },
+
+    -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.install").prefer_git = true
+            require("nvim-treesitter.configs").setup({
+                ensure_installed =  "all",
+                {
+--                    "json",
+--                    "vim",
+--                    "lua",
+--                    "rust",
+--                    "c",
+--                    "cpp",
+                },
+                highlight = { enable = true },
+            })
+        end
     },
 })
 
-require("nvim-treesitter.install").prefer_git = true
-
 require("options")
 require("keybindings")
-require("plugins/lsp/setup")
-require("plugins/lsp/cmp")
-require("plugins/lsp/nvim_treesitter")
+--require("plugins/lsp/setup")
+--require("plugins/lsp/cmp")
+--require("plugins/lsp/nvim_treesitter")
