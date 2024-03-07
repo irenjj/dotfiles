7538df6166a35d34c4d22bce8e92f32a2cea14ad
irenjj <renj.jiang@gmail.com>
2024-03-07 19:05:50 +0800

    nvim: add outline

--- nvim/init.lua
+++ nvim/init.lua
@@ -495,6 +495,18 @@ require("lazy").setup({
             vim.api.nvim_set_keymap("n", "<leader>c", ":DisableBlame<CR>", opt)
         end
     },
+
+    {
+        "hedyhli/outline.nvim",
+        lazy = true,
+        cmd = { "Outline", "OutlineOpen" },
+        keys = { -- Example mapping to toggle outline
+            { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+        },
+        opts = {
+          -- Your setup opts here
+        },
+    },
 })
 
 -- Reopen last Telescope window, super useful for live grep
