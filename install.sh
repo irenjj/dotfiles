619c1823fd03df24df5d8513e7c441a6251b7bbe
irenjj <renj.jiang@gmail.com>
2025-06-14 10:23:37 +0800

    delete alacritty ghostty vim zellij

--- install.sh
+++ install.sh
@@ -12,12 +12,6 @@ unlink ${HOME}/.bashrc
 ln -s ${CONF_DIR}/bash/.bash_profile ${HOME}/.bash_profile
 ln -s ${CONF_DIR}/bash/.bashrc ${HOME}/.bashrc
 
-# git 
-# mv ${HOME}/.gitconfig ${HOME}/.gitconfig.bak
-# mv ${HOME}/.gitignore_global ${HOME}/.gitignore_global.bak
-# cp ${CONF_DIR}/git/.gitconfig ${HOEM}/.gitconfig
-# cp ${CONF_DIR}/git/.gitignore_global ${HOEM}/.gitignore_global
-
 # tmux
 unlink ${HOME}/.tmux.conf
 ln -s ${CONF_DIR}/tmux/.tmux.conf ${HOME}/.tmux.conf
@@ -26,21 +20,9 @@ ln -s ${CONF_DIR}/tmux/.tmux.conf ${HOME}/.tmux.conf
 unlink ${HOME}/.lldbinit
 ln -s ${CONF_DIR}/lldb/.lldbinit ${HOME}/.lldbinit
 
-# alacritty
-unlink ${HOME}/.config/alacritty
-ln -s ${CONF_DIR}/alacritty ${HOME}/.config/alacritty
-
 # nvim
 unlink ${HOME}/.config/nvim
 ln -s ${CONF_DIR}/nvim ${HOME}/.config/nvim
-
-# vim
-unlink ${HOME}/.vimrc
-ln -s ${CONF_DIR}/vim/.vimrc ${HOME}/.vimrc
-
-# zellij
-unlink ${HOME}/.config/zellij
-ln -s ${CONF_DIR}/zellij ${HOME}/.config/zellij
 
 # kitty
 unlink ${HOME}/.config/kitty
